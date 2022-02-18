4ba285ba08549f2c4c98cee67caa77cfc78fa2de
mamidiajay11 <56168407+mamidiajay11@users.noreply.github.com>
2022-02-18 07:32:10 +0530

    firstchange

--- VMInstance/main.tf
+++ VMInstance/main.tf
@@ -1,7 +1,7 @@
 # This is the provider used to spin up the gcloud instance
 provider "google" {
  
-  project = "midevop"
+  project = "ajdevop"
   credentials = file("terraform.json")
   region  = "us-east1"
   
